5b6c0fdc2524bacc865482e7fbc9fb380d26d02f
PinYuan <c0919156850@gmail.com>
2021-01-23 18:01:26 +0800

    Function the script and extract noise from test set

--- egs/aurora4/s5/my_local/noise_distortion/prepare_noise_dir.py
+++ egs/aurora4/s5/my_local/noise_distortion/prepare_noise_dir.py
@@ -5,7 +5,15 @@ import subprocess
 from shutil import copyfile
 
 
-def main(kind):
+def dump_files(src_dir, out_dir, data):
+    with open(f"{out_dir}/wav.scp", "w") as file:
+        for key in sorted(data):
+            file.write(f"{key} {data[key]}\n")
+    copyfile(f"data/{src_dir}/utt2spk", f"{out_dir}/utt2spk")
+    copyfile(f"data/{src_dir}/spk2utt", f"{out_dir}/spk2utt")
+
+
+def prepare_train(kind):
     data_dir = f"data/train_si84_{kind}_sp_hires"
     os.makedirs(f"{data_dir}/wav", exist_ok=True)
     os.makedirs(f"{data_dir}/log", exist_ok=True)
@@ -32,17 +40,53 @@ def main(kind):
             log_file.write(f"{multi_key} {cmd}\n")
             
     # Dump wav.scp, utt2spk, spk2utt
-    with open(f"{data_dir}/wav.scp", "w") as file:
-        for key in sorted(data):
-            file.write(f"{key} {data[key]}\n")
-    copyfile(f"data/{multi_set}/utt2spk", f"{data_dir}/utt2spk")
-    copyfile(f"data/{multi_set}/spk2utt", f"{data_dir}/spk2utt")
+    dump_files(multi_set, data_dir, data)
+
+
+def prepare_test(kind):
+    clean_cmd_dict = dict()
+
+    for data_set in ["test_A_hires", "test_B_hires", "test_C_hires", "test_D_hires"]:
+        data_dir = "data/" + data_set.replace("_hires", f"_{kind}_hires")
+        os.makedirs(f"{data_dir}/wav", exist_ok=True)
+        os.makedirs(f"{data_dir}/log", exist_ok=True)
+
+        data = dict()
+
+        with open(f"data/{data_set}/wav.scp") as multi_file,\
+            open(f"{data_dir}/log/mix_wav.log", "w") as log_file:
+            for multi_line in tqdm.tqdm(multi_file):
+                multi_key, multi_cmd = multi_line.strip().split(" ", 1)
+                multi_cmd = multi_cmd[:-2]
+                
+                if "A" in data_set:
+                    clean_cmd_dict[multi_key] = multi_cmd
+                    # compute the difference between itself
+                    cmd = f"sox -m -v 1 '|{multi_cmd}' -v -1 '|{multi_cmd}' {data_dir}/wav/{multi_key}.wav"
+                else:
+                    clean_cmd = clean_cmd_dict[multi_key[:-1]+"0"]
+                    # compute the difference between 2 wav files and output a new wav
+                    cmd = f"sox -m -v 1 '|{multi_cmd}' -v -1 '|{clean_cmd}' {data_dir}/wav/{multi_key}.wav"
+                
+                p = subprocess.Popen(cmd, shell=True)
+                p.communicate()
+                data[multi_key] = f"{data_dir}/wav/{multi_key}.wav" # cmd
+
+                log_file.write(f"{multi_key} {cmd}\n")
+                
+        # Dump wav.scp, utt2spk, spk2utt
+        dump_files(data_set, data_dir, data)
+
+
+def main(kind):
+    prepare_train(kind)
+    prepare_test(kind)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("kind", default="noise_mismatch", nargs="?",
-                        help="'noise_mismatch' might contain channel distortion, "\ 
+                        help="'noise_mismatch' might contain channel distortion, "\
                              "'pure_noise' only extract added noise (TODO)")
     args = parser.parse_args()
 
